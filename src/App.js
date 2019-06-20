2aa47af3f6f6466f215b5ec1f16807fcc27dba4c
JackDing <361164217@qq.com>
2019-06-20 23:22:14 +0800

    引入redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {createStore} from 'redux'
 import Counter from './Counter'
 import './App.css';
 
+
+const reducer=(state,action)=>{
+    if(state){return 0}
+    switch (action.type) {
+        case 'plus':
+            return state+1
+        case 'minus':
+            return state-1
+    }
+}
+
+const store= createStore(reducer)
+
+
+
 function App() {
-  return (
+    const [state,changeState]=useState({
+        message:'haha',
+        number:5
+    })
+    return (
+
     <div className="App">
-      <Counter></Counter>
+      <Counter name={'haha'} number={state.number} store={store}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,19 @@
 import React,{useState} from 'react'
 
-function Counter() {
-    const [n,setn]=useState(0)
+function Counter(props) {
+    const [n,set]=useState(props.number)
     const plus=()=>{
-        setn(n+1)
+        set(n+1)
+    }
+    const sayHi=()=>{
+        alert('hello')
     }
     return(
         <div>
+            <div>{props.name}</div>
             <span>{n}</span>
             <button onClick={plus}>+1</button>
+            <button onClick={sayHi}>hi</button>
         </div>
     )
 }
