5be83083584eedd246f0ac0a61e1045048b0e4cf
JackDing <361164217@qq.com>
2019-06-22 22:43:27 +0800

    打包测试

---
+++ build/precache-manifest.05babccf51cfb06d73726b65b0405bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b4dd8100aff5a0eb37a8927c51e426",
+    "url": "/react-demo/build/index.html"
+  },
+  {
+    "revision": "d4301677d125ae41d24b",
+    "url": "/react-demo/build/static/css/main.983b725d.chunk.css"
+  },
+  {
+    "revision": "5a1a1c33922f7bc1604b",
+    "url": "/react-demo/build/static/js/2.3b03b8b3.chunk.js"
+  },
+  {
+    "revision": "d4301677d125ae41d24b",
+    "url": "/react-demo/build/static/js/main.9e660521.chunk.js"
+  },
+  {
+    "revision": "1f0c9ca927ddbc048117",
+    "url": "/react-demo/build/static/js/runtime~main.d65d2cfd.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-demo/dist/precache-manifest.566469314ebcbd37c34232e6f052577e.js"
+  "/react-demo/build/precache-manifest.05babccf51cfb06d73726b65b0405bc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-demo/dist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-demo/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
